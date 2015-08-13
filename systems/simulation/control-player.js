4529b2e46563867612e50f919a763a9b584cd074
Eric Lathrop <eric@ericlathrop.com>
2015-08-12 22:39:39 -0400

    Implement movement and controls

--- systems/simulation/control-player.js
+++ systems/simulation/control-player.js
@@ -1,20 +1,24 @@
 "use strict";
 
+var pawInSpeed = 0.5;
+var pawOutSpeed = -0.7;
+var pawMin = 300;
+var pawMax = 1000;
+
 module.exports = function(ecs, data) {
 	ecs.addEach(function(entity, elapsed) { // jshint ignore:line
 		entity.velocity.x = 0;
-		entity.velocity.y = 0;
-		if (data.input.button("left")) {
-			entity.velocity.x = -0.5;
+		entity.velocity.y = pawInSpeed;
+		if (data.input.button(entity.paw.input)) {
+			entity.velocity.y = pawOutSpeed;
 		}
-		if (data.input.button("right")) {
-			entity.velocity.x = 0.5;
-		}
-		if (data.input.button("up")) {
-			entity.velocity.y = -0.5;
-		}
-		if (data.input.button("down")) {
-			entity.velocity.y = 0.5;
-		}
-	}, ["player"]);
+      	if (entity.position.y < pawMin) {
+          entity.position.y = pawMin;
+          entity.velocity.y = 0;
+        }
+      	if (entity.position.y > pawMax) {
+          entity.position.y = pawMax;
+          entity.velocity.y = 0;
+        }
+	}, ["paw", "position", "size", "velocity"]);
 };
